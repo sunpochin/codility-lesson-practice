6a12ea1a3b44c27c07b8462dd5bd8391bb180fdd
Sun Pochin <sunpochin@gmail.com>
2018-04-28 00:44:14 +0800

    add files.

--- lesson5/countdiv.cpp
+++ lesson5/countdiv.cpp
@@ -8,13 +8,41 @@ int solution(int A, int B, int K) {
     // write your code in C++14 (g++ 6.2.0)
     
     int nReturn = 0;
-       
-    for (auto niter = A; niter <= B; ++niter) {
-        if (0 == niter % K ) {
-            ++nReturn;
-        }
-    }
-    return nReturn;
+    /// super slow, using division. 
+    // for (auto niter = A; niter <= B; ++niter) {
+    //     if (0 == niter % K ) {
+    //         ++nReturn;
+    //     }
+    // }
+
+    // a little bit faster, avoid using division.
+    // int niter = A;
+    // while(true) {
+    //     if (0 == niter % K ) {
+    //         ++nReturn;
+    //         niter += K;
+    //     } else {
+    //         ++niter;
+    //     }
+    //     // cout << " niter: " << niter << endl;
+    //     if (niter > B) {
+    //         // cout << " break. " << endl;
+    //         break;
+    //     }
+    // }
+
+    // fast, but not correct.
+    // int diff = B - A;
+    // int division = (B - A) / K;
+    // if (0 == A % K) {
+    //     division += 1;
+    // }
+    // nReturn = division;
+    // return nReturn;
+
+    int inclusive = (A%K == 0) ? 1 : 0;
+    return B/K - A/K + inclusive;
+    
 }
 
 
